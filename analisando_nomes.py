080d02049f9ba6b351d2c6b9e221c656317d32e0
EskurinhoES <eskurinhoes082023@gmail.com>
2024-01-27 09:38:40 -0300

    c

--- analisando_nomes.py
+++ analisando_nomes.py
@@ -2,6 +2,6 @@ nome = str(input('Digite seu primeiro nome: '))
 if len(nome) <= 4:
     print('Seu nome é curto!')
 elif len(nome) >= 5 and len(nome) <=6:
-    print('Seu nome é normal!')
+    print('Nome normal!')
 else:
     print('Seu nome é grande!')
